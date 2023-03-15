12181d92b2055a5ceb6dd28011ea47b10e9c1835
Tomash Mikulevich <61206454+tommikulevich@users.noreply.github.com>
2023-03-15 22:06:51 +0100

    Add project files.

---
+++ FormMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace MSN_GUI
+{
+    partial class FormMSN
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            openGLControlMain = new SharpGL.OpenGLControl();
+            openFileDialog = new OpenFileDialog();
+            menuStrip1 = new MenuStrip();
+            chooseObjToolStripMenuItem = new ToolStripMenuItem();
+            helpToolStripMenuItem = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)openGLControlMain).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // openGLControlMain
+            // 
+            openGLControlMain.DrawFPS = false;
+            openGLControlMain.Location = new Point(15, 47);
+            openGLControlMain.Margin = new Padding(6, 7, 6, 7);
+            openGLControlMain.Name = "openGLControlMain";
+            openGLControlMain.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            openGLControlMain.RenderContextType = SharpGL.RenderContextType.DIBSection;
+            openGLControlMain.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            openGLControlMain.Size = new Size(1127, 716);
+            openGLControlMain.TabIndex = 0;
+            // 
+            // openFileDialog
+            // 
+            openFileDialog.FileName = "openFileDialog";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(32, 32);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { chooseObjToolStripMenuItem, helpToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(1157, 40);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // chooseObjToolStripMenuItem
+            // 
+            chooseObjToolStripMenuItem.Name = "chooseObjToolStripMenuItem";
+            chooseObjToolStripMenuItem.Size = new Size(211, 36);
+            chooseObjToolStripMenuItem.Text = "Wybierz plik .obj";
+            chooseObjToolStripMenuItem.Click += chooseObjToolStripMenuItem_Click;
+            // 
+            // helpToolStripMenuItem
+            // 
+            helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            helpToolStripMenuItem.Size = new Size(106, 36);
+            helpToolStripMenuItem.Text = "Pomóc";
+            helpToolStripMenuItem.Click += helpToolStripMenuItem_Click;
+            // 
+            // FormMSN
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1157, 779);
+            Controls.Add(menuStrip1);
+            Controls.Add(openGLControlMain);
+            MainMenuStrip = menuStrip1;
+            Name = "FormMSN";
+            Text = "Projekt MSN";
+            Load += FormMain_Load;
+            ((System.ComponentModel.ISupportInitialize)openGLControlMain).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControlMain;
+        private OpenFileDialog openFileDialog;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem chooseObjToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+    }
+}
---
+++ FormMain.cs
@@ -0,0 +1,198 @@
+using SharpGL;
+using Assimp;
+
+
+namespace MSN_GUI
+{
+    public partial class FormMSN : Form
+    {
+        // Graphics parametres
+        private OpenGLControl openGLControl => openGLControlMain;
+        private Scene model;
+
+        // Object parametres
+        private Vector3D objectCenter;
+        private Vector3D objectSize;
+
+        // Rotating parametres
+        private float rotationX, rotationY;
+        private Point lastMousePosition;
+
+        public FormMSN()
+        {
+            InitializeComponent();
+        }
+
+        // Choosing .obj file
+        private void chooseObjToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.Filter = "OBJ Files (*.obj)|*.obj";
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openFileDialog.FileName;
+                    AssimpContext importer = new AssimpContext();
+                    model = importer.ImportFile(filePath, PostProcessPreset.TargetRealTimeMaximumQuality);
+
+                    // Calculate bounding box, center and size of the object
+                    calculateBoundingBox(model, out Vector3D min, out Vector3D max, out Vector3D size);
+                    objectCenter = (max + min) / 2;
+                    objectSize = size;
+                }
+            }
+        }
+
+        // Help section
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Tutaj nied³ugo pojawi siê pomóc! Proszê czekaæ ...";
+            string title = "Pomóc";
+            MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        // Calculating object bounding box to set object correctly on the screen
+        private void calculateBoundingBox(Scene model, out Vector3D min, out Vector3D max, out Vector3D size)
+        {
+            min = new Vector3D(float.MaxValue, float.MaxValue, float.MaxValue);
+            max = new Vector3D(float.MinValue, float.MinValue, float.MinValue);
+
+            if (model != null)
+            {
+                foreach (var mesh in model.Meshes)
+                {
+                    foreach (var vertex in mesh.Vertices)
+                    {
+                        min.X = Math.Min(min.X, vertex.X);
+                        min.Y = Math.Min(min.Y, vertex.Y);
+                        min.Z = Math.Min(min.Z, vertex.Z);
+
+                        max.X = Math.Max(max.X, vertex.X);
+                        max.Y = Math.Max(max.Y, vertex.Y);
+                        max.Z = Math.Max(max.Z, vertex.Z);
+                    }
+                }
+            }
+
+            size = max - min;
+        }
+
+        // Loading the form 
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            openGLControl.OpenGLInitialized += openGLControlMain_OpenGLInitialized!;
+            openGLControl.OpenGLDraw += openGLControlMain_OpenGLDraw!;
+
+            openGLControl.MouseDown += openGLControlMain_MouseDown!;
+            openGLControl.MouseMove += openGLControlMain_MouseMove!;
+        }
+
+        // Graphics initialization
+        private void openGLControlMain_OpenGLInitialized(object sender, EventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+
+            gl.LoadIdentity();
+            gl.Perspective(45.0f, (double)openGLControl.Width / (double)openGLControl.Height, 1, 1000);
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            gl.Enable(OpenGL.GL_DEPTH_TEST);
+
+            // Lighting
+            gl.Enable(OpenGL.GL_LIGHTING);
+            gl.Enable(OpenGL.GL_LIGHT0);
+            gl.Enable(OpenGL.GL_COLOR_MATERIAL);
+
+            gl.ShadeModel(OpenGL.GL_SMOOTH);
+        }
+
+        // Graphics drawing and refreshing
+        private void openGLControlMain_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            OpenGL gl = openGLControl.OpenGL;
+            float scaleFactor = 0.05f;
+
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+            gl.LoadIdentity();
+
+            // Camera
+            float sizeFactor = Math.Max(objectSize.X, Math.Max(objectSize.Y, objectSize.Z)) * scaleFactor;
+            float cameraDist = 2.5f * sizeFactor;
+            gl.LookAt(0, 0, cameraDist, 0, 0, 0, 0, 1, 0);
+
+            // Rotation
+            gl.Rotate(rotationX, 1.0f, 0.0f, 0.0f);
+            gl.Rotate(rotationY, 0.0f, 1.0f, 0.0f);
+
+            // Scale and center the object
+            gl.Scale(scaleFactor, scaleFactor, scaleFactor);
+            gl.Translate(-objectCenter.X, -objectCenter.Y, -objectCenter.Z);
+
+            if (model != null)
+            {
+                for (int i = 0; i < model.Meshes.Count; i++)
+                {
+                    var mesh = model.Meshes[i];
+                    var material = model.Materials[mesh.MaterialIndex];
+
+                    if (material != null)
+                    {
+                        if (material.HasColorDiffuse)
+                        {
+                            var color = material.ColorDiffuse;
+                            gl.Color(color.R, color.G, color.B, color.A);
+                        }
+                    }
+
+                    gl.Begin(OpenGL.GL_TRIANGLES);
+                    foreach (var face in mesh.Faces)
+                    {
+                        foreach (var index in face.Indices)
+                        {
+                            var vertex = mesh.Vertices[index];
+                            gl.Vertex(vertex.X, vertex.Y, vertex.Z);
+
+                            if (mesh.HasNormals)
+                            {
+                                var normal = mesh.Normals[index];
+                                gl.Normal(normal.X, normal.Y, normal.Z);
+                            }
+
+                            if (mesh.HasTextureCoords(0))
+                            {
+                                var texCoord = mesh.TextureCoordinateChannels[0][index];
+                                gl.TexCoord(texCoord.X, texCoord.Y);
+                            }
+                        }
+                    }
+                    gl.End();
+                }
+            }
+
+            gl.Flush();
+        }
+
+        // Mouse behaviour I
+        private void openGLControlMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastMousePosition = e.Location;
+        }
+
+        // Mouse behaviour II
+        private void openGLControlMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                float dx = e.X - lastMousePosition.X;
+                float dy = e.Y - lastMousePosition.Y;
+
+                rotationY += dx * 0.5f;
+                rotationX += dy * 0.5f;
+
+                openGLControl.Invalidate();
+            }
+
+            lastMousePosition = e.Location;
+        }
+    }
+}
