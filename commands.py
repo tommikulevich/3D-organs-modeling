d4b0f19e9238d4b9c214665738c796de58326e6c
Tomash Mikulevich <tommikulevich@gmail.com>
2023-06-10 19:10:11 +0100

    Remove user paths.

--- commands.py
+++ commands.py
@@ -1,5 +1,5 @@
-dicomDataDir= "C:/Users/Misio/PycharmProjects/MONAIv2/20190514_HipLarge_Angio CT/Hip Large (1024x1024x834)"
-outputFolder= "D:/stl_testy_slicer/Nowy folder (8)"
+dicomDataDir= ""
+outputFolder= ""
 
 loadedNodeIDs = []
 from DICOMLib import DICOMUtils
